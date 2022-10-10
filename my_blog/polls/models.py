04010011f0858495cdbdea5a747512ba9c5cdafa
Hubert798 <113973105+Hubert798@users.noreply.github.com>
2022-10-10 22:51:23 +0200

    Add files via upload

--- my_blog/polls/models.py
+++ my_blog/polls/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
-
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200, null=True)
+    pub_date = models.DateTimeField("date published")
 
+    def __str__(self):
+        return self.question_text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.answer
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
 
-class Answers(models.Model):
-    uestion = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.choice_text
+
--- my_blog/polls/views.py
+++ my_blog/polls/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
-# Create your views here.
+def get_question_list(request):
+    question = Question.objects.all()
+    context = {
+        "question": question
+    }
+    return render(template_name='question_list.html', request=request, context=context)
+
+def get_question_detail(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    context = {
+        "question": question
+    }
+    return render(template_name='question_detail.html', request=request, context=context)
+
